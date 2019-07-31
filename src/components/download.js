2ddc6d9af863c05455214808780cd6bfc0125250
Ashley Jaynes <ashley.jaynes@mac.com>
2019-07-30 18:23:43 -0700

    fixed resume download issues and configured favicons that were broken

--- src/components/download.js
+++ src/components/download.js
@@ -1,15 +1,12 @@
 import React from "react"
+import resume from "../images/resume2019.pdf"
 
 const Download = () => {
   return (
     <>
       <div className="break-centered" />
       <div className="card-content has-text-centered">
-        <a
-          className="button is-danger is-medium"
-          download
-          href="resume2019.pdf"
-        >
+        <a className="button is-danger is-medium" download href={resume}>
           Download My Resume
         </a>
       </div>
